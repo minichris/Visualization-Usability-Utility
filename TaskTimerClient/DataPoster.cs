8d6111814b16c85e2e427f9e62267a186184665d
minichris <minichris502@gmail.com>
2019-08-31 01:26:42 +0100

    Threw together a server for holding the info generated by the clients

--- TaskTimerClient/DataPoster.cs
+++ TaskTimerClient/DataPoster.cs
@@ -1,23 +1,23 @@
 ﻿using RestSharp;
+using System;
 
 namespace TaskTimer
 {
     class DataPoster
     {
-        public static void SendData(StudyTask Task)
+        public static IRestResponse SendData(StudyTask Task)
         {
-            var client = new RestClient("https://webhook.site/1b903b14-cb31-455c-a7e0-059356b6896c");
-            var request = new RestRequest("post", Method.POST);
+            var client = new RestClient("http://localhost:3000/");
+            var request = new RestRequest("tasks", Method.POST);
             request.RequestFormat = DataFormat.Json;
-            request.AddFileBytes("Screenshot", Task.ScreenshotPNG, "Screenshot.png", "image/png");
-
-            //request.AddParameter("Screenshot", System.Convert.ToBase64String(Task.ScreenshotPNG));
             request.AddParameter("Title", Task.Title);
             request.AddParameter("System", Task.TaskURL);
             request.AddParameter("TimeSpent", Task.TimeSpentOnTask);
             request.AddParameter("BelievesSuccess", Task.ParticipantBelievesSuccess);
+            request.AddParameter("Screenshot", System.Convert.ToBase64String(Task.ScreenshotPNG));
 
-            var response = client.Execute(request);
+            IRestResponse response = client.Execute(request);
+            return response;
         }
     }
 }
